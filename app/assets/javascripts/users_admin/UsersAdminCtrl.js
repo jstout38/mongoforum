9463dbecc4749ea3bbb645f0667b4850dc355254
Justin Stout <jstout38@gmail.com>
2016-09-10 19:50:44 -0400

    Commented and formatted UsersAdmin assets

--- app/assets/javascripts/users_admin/UsersAdminCtrl.js
+++ app/assets/javascripts/users_admin/UsersAdminCtrl.js
@@ -1,62 +1,51 @@
 angular.module('stoutForum')
-.controller('UsersAdminCtrl', [
- '$scope',
- '$state',
- 'users_admin', 
- 'Auth',
- 'user',
- '$stateParams',
- function($scope, $state, users_admin, Auth, user, $stateParams){   
-   $scope.user = user.data;
-   $scope.user.birthday = new Date($scope.user.birthday);          
-   $scope.users_admin = users_admin.users_admin;
-   $scope.current_page = $stateParams.page;   
-   $scope.total_users = users_admin.userCount;
-   $scope.hasAvatar = function() {
-         if ($scope.user.avatar != null && $scope.user.avatar != "") {
-            return true;
-         }
-         return false;
-      };   
-   $scope.update = function() {
- 			users_admin.update($scope.user, Auth._currentUser).then(function(){
- 				$state.go('home');
- 			});
- 		};
-   $scope.pagesArray = function(total_users){
-      var pages = [];
-      var pageCount = Math.floor((total_users - 1) / 10 ) + 1;
-      if (pageCount > 6 && $scope.current_page < 3) {
-         pages = [1, 2, 3, "...", pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page == 3) {
-         pages = [1, 2, 3, 4, "...", pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page >= 4 && $scope.current_page < pageCount - 2) {
-         pages = [1, "...", $scope.current_page - 1, $scope.current_page, parseInt($scope.current_page) + 1, "...", pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page == pageCount - 2) {
-         pages = [1, "...", $scope.current_page -1, pageCount - 2, pageCount - 1, pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page > pageCount - 2) {
-         pages = [1, "...", pageCount - 2, pageCount - 1, pageCount];
-      }
-      else {
-         for (var i = 1; i <= pageCount; i++) {
-            pages.push(i);
-         }
-      }
-      return pages;
-   };  
-   
-}
-   //$scope.addPost = function(){
-   //	 if(!$scope.title || $scope.title === '') { return; }
-   //	 $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0, comments: [ {author: 'Joe', body: 'Cool post!', upvotes: 0}, {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}]});
-   //	 $scope.title = '';
-   //	 $scope.link = '';
-   //};
-   //$scope.incrementUpvotes = function(post) {
-   //  post.upvotes += 1;
-   
- ]);
+    .controller('UsersAdminCtrl', [
+        '$scope',
+        '$state',
+        'users_admin',
+        'Auth',
+        'user',
+        '$stateParams',
+        function ($scope, $state, users_admin, Auth, user, $stateParams) {
+            //Set user and users_admin to the resolved data from the routing
+            $scope.user = user.data;
+            $scope.user.birthday = new Date($scope.user.birthday);
+            $scope.users_admin = users_admin.users_admin;
+            $scope.current_page = $stateParams.page;
+            $scope.total_users = users_admin.userCount;
+            $scope.hasAvatar = function () {
+                //Check if the user has an avatar
+                if ($scope.user.avatar !== null && $scope.user.avatar !== "") {
+                    return true;
+                }
+                return false;
+            };
+            $scope.update = function () {
+                //Update the user's account and then navigate home
+                users_admin.update($scope.user, Auth._currentUser).then(function () {
+                    $state.go('home');
+                });
+            };
+            $scope.pagesArray = function (total_users) {
+                //Helper function that returns an array for the page navigation
+                var pages = [];
+                var pageCount = Math.floor((total_users - 1) / 10) + 1;
+                if (pageCount > 6 && $scope.current_page < 3) {
+                    pages = [1, 2, 3, "...", pageCount];
+                } else if (pageCount > 6 && $scope.current_page === 3) {
+                    pages = [1, 2, 3, 4, "...", pageCount];
+                } else if (pageCount > 6 && $scope.current_page >= 4 && $scope.current_page < pageCount - 2) {
+                    pages = [1, "...", $scope.current_page - 1, $scope.current_page, parseInt($scope.current_page) + 1, "...", pageCount];
+                } else if (pageCount > 6 && $scope.current_page == pageCount - 2) {
+                    pages = [1, "...", $scope.current_page -1, pageCount - 2, pageCount - 1, pageCount];
+                } else if (pageCount > 6 && $scope.current_page > pageCount - 2) {
+                    pages = [1, "...", pageCount - 2, pageCount - 1, pageCount];
+                } else {
+                    for (var i = 1; i <= pageCount; i++) {
+                        pages.push(i);
+                    }
+                }
+                return pages;
+            };
+        }
+    ]);
--- app/assets/javascripts/users_admin/users_admin.js
+++ app/assets/javascripts/users_admin/users_admin.js
@@ -1,34 +1,26 @@
 angular.module('stoutForum')
-.factory('users_admin', [
-  '$http',
-  function($http){
-  	var o = { users_admin: [], userCount: 0 };
-  	o.getAll = function(page) {
-  	  return $http.get('/users_admin/index/' + page).success(function(data){  	  	
-        angular.copy(data.users, o.users_admin);
-        o.userCount = data.userCount;        
-  	  });
-  	};
-    o.update = function(body, user) {
-      return $http.put('/users_admin/' + user._id + '.json', body).success(function(data){
-        
-      });
-    };
-    o.get = function(user) {
-      return $http.get('/users_admin/' + user + '.json').success(function(data){        
-        return data;
-      });
-    }    
-  	return o;
-  }])
-/*.factory('sub_forum', [
-  '$http',
-  function($http){
-    var o
-    o.get = function(id) {
-      return $http.get('/sub_forums/' + id + '.json').then(function(res){
-        return res.data;
-      });
-    };
-    return o;
-  }]);*/
+    .factory('users_admin', [
+        '$http',
+        function ($http) {
+            //Set a variable for holding the current pages posts and post counts
+            var o = {users_admin: [], userCount: 0};
+            o.getAll = function (page) {
+                //Call the API to get all users
+                return $http.get('/users_admin/index/' + page).success(function (data) {
+                    angular.copy(data.users, o.users_admin);
+                    o.userCount = data.userCount;
+                });
+            };
+            o.update = function (body, user) {
+                //Call the API to update the user
+                return $http.put('/users_admin/' + user._id + '.json', body);
+            };
+            o.get = function (user) {
+                //Call the API to get a single user
+                return $http.get('/users_admin/' + user + '.json').success(function (data) {
+                    return data;
+                });
+            };
+            return o;
+        }
+    ]);
