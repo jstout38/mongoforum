966803704ab874683cfb2ca8f9bfa9bba4e0ecfc
Justin Stout <jstout38@gmail.com>
2016-09-10 18:39:49 -0400

    Commented and formatted the Auth assets

--- app/assets/javascripts/auth/authCtrl.js
+++ app/assets/javascripts/auth/authCtrl.js
@@ -1,37 +1,44 @@
 angular.module('stoutForum')
- .controller('AuthCtrl', [
- 	'$scope',
- 	'$state',
- 	'Auth',
- 	function($scope, $state, Auth){
- 		$scope.user = {}
- 		$scope.user.birthday = new Date();
- 		$scope.hasAvatar = function() {
- 			if ($scope.user.avatar != null && $scope.user.avatar != "") {
- 				return true;
- 			}
- 			return false;
- 		};
- 		$scope.clearError = function() {
- 			$(".errorMessages").hide();
- 		};
- 		$scope.showError = function() {
- 			$(".errorMessages").show();
- 		};
- 		$scope.login = function() {
- 			Auth.login($scope.user).then(function(){
- 				$state.go('home');
- 			}, function(errors){
- 				$scope.responseMessage = errors.data;
- 			});
- 		};
- 		$scope.register = function() { 			
- 			Auth.register($scope.user).then(function(){
- 				$state.go('home');
- 			}, function(errors){
- 				$scope.emailResponses = errors.data.errors["email"];
- 				$scope.usernameResponses = errors.data.errors["username"];
- 				$scope.passwordResponses = errors.data.errors["password"]; 				
- 			});
- 		};
- 	}]);
+    .controller('AuthCtrl', [
+        '$scope',
+        '$state',
+        'Auth',
+        function ($scope, $state, Auth) {
+            //Initialize user hash and date object for birthdate
+            $scope.user = {};
+            $scope.user.birthday = new Date();
+            $scope.hasAvatar = function () {
+                //Check if the user has an avatar
+                if ($scope.user.avatar !== null && $scope.user.avatar !== "") {
+                    return true;
+                }
+                return false;
+            };
+            $scope.clearError = function () {
+                //Function to be called when errors must be cleared
+                $(".errorMessages").hide();
+            };
+            $scope.showError = function () {
+                //Function to be called when errors must be shown
+                $(".errorMessages").show();
+            };
+            $scope.login = function () {
+                //Use Devise Auth to log a user in using the hash provided by the form then go home, or provide errors to the client
+                Auth.login($scope.user).then(function () {
+                    $state.go('home');
+                }, function (errors) {
+                    $scope.responseMessage = errors.data;
+                });
+            };
+            $scope.register = function () {
+                //Use Devise Auth to register a user using the hash provided by the form then go home, or provide errors to the client
+                Auth.register($scope.user).then(function () {
+                    $state.go('home');
+                }, function (errors) {
+                    $scope.emailResponses = errors.data.errors.email;
+                    $scope.usernameResponses = errors.data.errors.username;
+                    $scope.passwordResponses = errors.data.errors.password;
+                });
+            };
+        }
+    ]);
