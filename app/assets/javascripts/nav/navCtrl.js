a33c02319cda408cbff7828a5da9bc91d22f78bf
Justin Stout <jstout38@gmail.com>
2016-09-11 15:42:40 -0400

    Commented and Formatted Nav and Post assets

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -1,23 +1,28 @@
 angular.module('stoutForum')
- .controller('NavCtrl', [
- 	'$scope',
- 	'$state',
- 	'Auth',
- 	function($scope, $state, Auth){
-
- 		$scope.signedIn = Auth.isAuthenticated;
- 		$scope.logout = Auth.logout;
- 		Auth.currentUser().then(function (user){
- 			$scope.user = user;
- 		});
- 		$scope.$on('devise:new-registration', function (e, user){
- 			$scope.user = user;
- 		});
- 		$scope.$on('devise:login', function (e, user){
- 			$scope.user = user;
- 		});
- 		$scope.$on('devise:logout', function (e, user){
- 			$scope.user = {};
- 			$state.go('home');
- 		})
- 	}]);
+    .controller('NavCtrl', [
+        '$scope',
+        '$state',
+        'Auth',
+        function ($scope, $state, Auth) {
+            //Set scope variables using Devise Auth module
+            $scope.signedIn = Auth.isAuthenticated;
+            $scope.logout = Auth.logout;
+            Auth.currentUser().then(function (user) {
+                //Set the current user using Devise Auth module
+                $scope.user = user;
+            });
+            $scope.$on('devise:new-registration', function (e, user) {
+                //Set the current user when the user registers
+                $scope.user = user;
+            });
+            $scope.$on('devise:login', function (e, user) {
+                //Set the current user when the user logs in
+                $scope.user = user;
+            });
+            $scope.$on('devise:logout', function (e, user) {
+                //Clear the current user and go home when the user logs out
+                $scope.user = {};
+                $state.go('home');
+            });
+        }
+    ]);
--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,65 +1,66 @@
 angular.module('stoutForum')
-.factory('posts', [
-  	'$http',    
-  function($http){
-  	var o = { posts: [] };
-  	o.getAll = function(sub_forum_id, id, page) {
-      return $http.get('/sub_forums/' + sub_forum_id + '/forum_threads/' + id + '/posts/index/' + page + '.json').success(function(data){        
-        angular.copy(data, o.posts);
-      })
-    };
-    //o.get = function(id, sub_forum) {
-  	//  return $http.get('/forum_threads/' + id + '.json').then(function(res){
-  	//  	return res.data;
-  	//  })
-  	//};
-    o.create = function(body, forum_thread, last_page) {
-      return $http.post('/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/posts.json', body).success(function(data){
-        if (o.posts.length > 9) {          
-          window.location.href = '#/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/' + last_page;
+    .factory('posts', [
+        '$http',
+        function ($http) {
+            //Initialize an array for posts to be placed in
+            var o = {posts: []};
+            o.getAll = function (sub_forum_id, id, page) {
+                //Call the API to retrieve all posts for the current thread
+                return $http.get('/sub_forums/' + sub_forum_id + '/forum_threads/' + id + '/posts/index/' + page + '.json').success(function (data) {
+                    angular.copy(data, o.posts);
+                });
+            };
+            o.create = function (body, forum_thread, last_page) {
+                //Call the API to create a new post
+                return $http.post('/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/posts.json', body).success(function (data) {
+                    //Redirect to the new page if the new post starts a new page
+                    if (o.posts.length > 9) {
+                        window.location.href = '#/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/' + last_page;
+                    }
+                    //Add the post to the current page and update the post count for the poster on the page
+                    o.posts.push(data);
+                    var currentUser = o.posts[o.posts.length - 1].user.id;
+                    for (post in o.posts) {
+                        if (post.upvotes.id == currentUser) {
+                            post.user.postCount++;
+                        }
+                    }
+                });
+            };
+            o.upvote = function (forum_thread, post) {
+                //Call the API to update a post's votes based on an upvote
+                return $http.put('/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/posts/' + post._id + '/upvote.json').then(function (data) {
+                    if (data.status == 200) {
+                        post.upvotes += 1;
+                    }
+                });
+            };
+            o.downvote = function (forum_thread, post) {
+                //Call the API to update a post's votes based on a downvote
+                return $http.put('/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/posts/' + post._id + '/downvote.json').then(function (data) {
+                    if (data.status == 200) {
+                        post.downvotes += 1;
+                    }
+                });
+            };
+            o.search = function (search_hash, post_page, thread_page) {
+                //Clean up the hash results so that they work properly with the API
+                var keyword = search_hash.post_search;
+                var user = search_hash.user_search;
+                var time = search_hash.time;
+                var topic = search_hash.thread_search;
+                if (search_hash.post_search == "") {
+                    keyword = "undefined";
+                }
+                if (search_hash.user_search == "") {
+                    user = "undefined";
+                }
+                if (search_hash.thread_search == "") {
+                    topic = "undefined";
+                }
+                //Call the API to get results based on the above parameters
+                return $http.get('/search/' + post_page + '/' + thread_page + '/' + time + '/' + keyword + '/' + user + '/' + topic);
+            };
+            return o;
         }
-        o.posts.push(data);
-        var currentUser = o.posts[o.posts.length - 1].user.id;
-        for (post in o.posts) {
-          if (post.upvotes.id == currentUser) {
-            post.user.postCount++;
-          }
-        }        
-      });
-    };
-    o.upvote = function(forum_thread, post) {
-      return $http.put('/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/posts/' + post._id + '/upvote.json')
-        .then(function(data){
-          if (data.status == 200) {
-            post.upvotes += 1;
-          }
-        });
-    };
-    o.downvote = function(forum_thread, post) {
-      return $http.put('/sub_forums/' + forum_thread.sub_forum_id + '/forum_threads/' + forum_thread._id + '/posts/' + post._id + '/downvote.json')
-        .then(function(data){
-          if (data.status == 200) {
-            post.downvotes += 1;
-          }
-        });
-    };
-    o.search = function(search_hash, post_page, thread_page) {
-      var keyword = search_hash.post_search;
-      var user = search_hash.user_search;
-      var time = search_hash.time;
-      var topic = search_hash.thread_search;
-      if (search_hash.post_search == "") {
-        keyword = "undefined";
-      }
-      if (search_hash.user_search == "") {
-        user = "undefined";
-      }
-      if (search_hash.thread_search == "") {
-        topic = "undefined";
-      }
-      return $http.get('/search/' + post_page + '/' + thread_page + '/' + time + '/' + keyword + '/' + user + '/' + topic).success(function(data){        
-        //o.posts.push(data);
-      });
-    }
-  	return o;
-  }]);
+    ]);
