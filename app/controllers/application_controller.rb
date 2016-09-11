fd4d5f383a25aa8c6bc9dbf62f79349120f9a9b1
Justin Stout <jstout38@gmail.com>
2016-09-11 16:41:12 -0400

    Turned security tokens back on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   skip_before_filter  :verify_authenticity_token
 
   respond_to :json
@@ -9,13 +9,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def angular
-  	render 'layouts/application'
+      render 'layouts/application'
   end
 
   private
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :username, :website, :facebook, :twitter, :pinterest, :linked_in, :location, :occupation, :interests, :birthday, :signature, :avatar) }
-    devise_parameter_sanitizer.for(:sign_in) << :username
+  	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :username, :website, :facebook, :twitter, :pinterest, :linked_in, :location, :occupation, :interests, :birthday, :signature, :avatar) }
+      devise_parameter_sanitizer.for(:sign_in) << :username
   end
 
 end
