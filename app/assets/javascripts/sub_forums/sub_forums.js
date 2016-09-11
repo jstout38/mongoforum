f76dc88990b154a04f21f5bb58ab9f2841cef87c
Justin Stout <jstout38@gmail.com>
2016-09-11 14:52:23 -0400

    Commented and formatted the SubForums assets

--- app/assets/javascripts/sub_forums/sub_forums.js
+++ app/assets/javascripts/sub_forums/sub_forums.js
@@ -1,28 +1,21 @@
 angular.module('stoutForum')
-.factory('sub_forums', [
-  '$http',
-  function($http){
-  	var o = { sub_forums: [] };
-  	o.getAll = function() {
-  	  return $http.get('/sub_forums.json').success(function(data){
-  	  	angular.copy(data, o.sub_forums)
-  	  })
-  	};
-    o.get = function(id) {
-      return $http.get('/sub_forums/' + id + '.json').then(function(res){
-        return res.data
-      });
-    };
-  	return o;
-  }])
-/*.factory('sub_forum', [
-  '$http',
-  function($http){
-    var o
-    o.get = function(id) {
-      return $http.get('/sub_forums/' + id + '.json').then(function(res){
-        return res.data;
-      });
-    };
-    return o;
-  }]);*/
+    .factory('sub_forums', [
+        '$http',
+        function ($http) {
+            //Initialize an empty array to hold SubForums
+            var o = {sub_forums: []};
+            o.getAll = function () {
+                //Call the API to get an array of all SubForums
+                return $http.get('/sub_forums.json').success(function (data) {
+                    angular.copy(data, o.sub_forums);
+                });
+            };
+            o.get = function (id) {
+                //Call the API to get one SubForum with the passed id
+                return $http.get('/sub_forums/' + id + '.json').then(function (res) {
+                    return res.data;
+                });
+            };
+            return o;
+        }
+    ]);
