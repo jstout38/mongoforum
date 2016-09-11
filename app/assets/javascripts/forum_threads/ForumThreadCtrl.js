56404821d0df95d5814c67e4064c8fef3d0f9d04
Justin Stout <jstout38@gmail.com>
2016-09-11 16:26:41 -0400

    Commented and Formatted Home and ForumThread Assets

--- app/assets/javascripts/forum_threads/ForumThreadCtrl.js
+++ app/assets/javascripts/forum_threads/ForumThreadCtrl.js
@@ -1,119 +1,111 @@
 angular.module('stoutForum')
-.controller('ForumThreadCtrl', [
- '$scope',
- 'forum_threads',
- 'forum_thread',
- 'posts',
- 'Auth',
- '$stateParams',
- '$location',
- '$anchorScroll',
- '$document',
- function($scope, forum_threads, forum_thread, posts, Auth, $stateParams, $location, $anchorScroll, $document){
-   
-   $scope.forum_thread = forum_thread;
-   $scope.posts = posts.posts;
-   $scope.current_page = $stateParams.page;
-   $scope.showAllPosts = false;
-   $scope.posts_in_thread = forum_thread.posts.length;   
-        
-   $document.ready(function(){
-      urlParts = $location.absUrl().split('#');
-      if (urlParts.length > 2) {      
-         $location.hash(urlParts[2]);
-         $anchorScroll();      
-      }
-   });
-   $scope.addPost = function(){      
-      if(!$scope.body || $scope.body === '') {return;}
-      last_page = Math.floor( $scope.posts_in_thread / 10) + 1;      
-      posts.create({
-         body: $scope.body         
-      }, $scope.forum_thread, last_page);
-      $scope.body = '';
-      $scope.posts_in_thread++;
-   };
-   $scope.hasAvatar = function(post){
-      if (post.user.avatar == "") {
-         return false;
-      }      
-      return true;
-   };
-   $scope.hasTwitter = function(post){
-      if (post.user.twitter == "") {
-         return false;
-      }
-      return true;
-   };
-   $scope.hasFacebook = function(post){
-      if (post.user.facebook == "") {
-         return false;
-      }
-      return true;
-   };
-   $scope.hasPinterest = function(post){
-      if (post.user.pinterest == "") {
-         return false;
-      }
-      return true;
-   };
-   $scope.hasLinkedIn = function(post){
-      if (post.user.linked_in == "") {
-         return false;
-      }
-      return true;
-   };
-   $scope.hasSignature = function(post){
-      if (post.user.signature == "") {
-         return false;
-      }
-      return true;
-   };
-   $scope.incrementUpvotes = function(forum_thread, post) {
-       posts.upvote(forum_thread, post);
-     };
-   $scope.incrementDownvotes = function(forum_thread, post) {
-      posts.downvote(forum_thread, post);
-   };
-   $scope.isLoggedIn = function(){
-      return Auth.isAuthenticated();   
-   };
-   $scope.pagesArray = function(thread){
-      var pages = [];
-      var pageCount = Math.floor((thread.posts.length - 1) / 10 ) + 1;
-      if (pageCount > 6 && $scope.current_page < 3) {
-         pages = [1, 2, 3, "...", pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page == 3) {
-         pages = [1, 2, 3, 4, "...", pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page >= 4 && $scope.current_page < pageCount - 2) {
-         pages = [1, "...", $scope.current_page - 1, $scope.current_page, parseInt($scope.current_page) + 1, "...", pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page == pageCount - 2) {
-         pages = [1, "...", $scope.current_page -1, pageCount - 2, pageCount - 1, pageCount];
-      }
-      else if (pageCount > 6 && $scope.current_page > pageCount - 2) {
-         pages = [1, "...", pageCount - 2, pageCount - 1, pageCount];
-      }
-      else {
-         for (var i = 1; i <= pageCount; i++) {
-            pages.push(i);
-         }
-      }
-      return pages;
-   };
-   $scope.showPosts = function(container){
-      $scope.showAllPosts = true;
-   };
-   //$scope.addPost = function(){
-   //	 if(!$scope.title || $scope.title === '') { return; }
-   //	 $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0, comments: [ {author: 'Joe', body: 'Cool post!', upvotes: 0}, {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}]});
-   //	 $scope.title = '';
-   //	 $scope.link = '';
-   //};
-   //$scope.incrementUpvotes = function(post) {
-   //  post.upvotes += 1;
-
-   }
- ]);
+    .controller('ForumThreadCtrl', [
+        '$scope',
+        'forum_thread',
+        'posts',
+        'Auth',
+        '$stateParams',
+        '$location',
+        '$anchorScroll',
+        '$document',
+        function ($scope, forum_thread, posts, Auth, $stateParams, $location, $anchorScroll, $document) {
+            //Set scope variables based on URL params and resolved variables
+            $scope.forum_thread = forum_thread;
+            $scope.posts = posts.posts;
+            $scope.current_page = $stateParams.page;
+            $scope.showAllPosts = false;
+            $scope.posts_in_thread = forum_thread.posts.length;
+            //Navigate to anchor if provided
+            $document.ready(function () {
+                var urlParts = $location.absUrl().split('#');
+                if (urlParts.length > 2) {
+                    $location.hash(urlParts[2]);
+                    $anchorScroll();
+                }
+            });
+            $scope.addPost = function () {
+                //Add the post provided by the form hash and increment the thread post counter
+                if (!$scope.body || $scope.body === '') {return;}
+                last_page = Math.floor($scope.posts_in_thread / 10) + 1;
+                posts.create({
+                    body: $scope.body
+                }, $scope.forum_thread, last_page);
+                $scope.body = '';
+                $scope.posts_in_thread++;
+            };
+            //The following functions check if the user fields are currently blank or not
+            $scope.hasAvatar = function (post) {
+                if (post.user.avatar == "") {
+                    return false;
+                }
+                return true;
+            };
+            $scope.hasTwitter = function (post) {
+                if (post.user.twitter == "") {
+                    return false;
+                }
+                return true;
+            };
+            $scope.hasFacebook = function (post) {
+                if (post.user.facebook == "") {
+                    return false;
+                }
+                return true;
+            };
+            $scope.hasPinterest = function (post) {
+                if (post.user.pinterest == "") {
+                    return false;
+                }
+                return true;
+            };
+            $scope.hasLinkedIn = function (post) {
+                if (post.user.linked_in == "") {
+                    return false;
+                }
+                return true;
+            };
+            $scope.hasSignature = function (post) {
+                if (post.user.signature == "") {
+                    return false;
+                }
+                return true;
+            };
+            $scope.incrementUpvotes = function (forum_thread, post) {
+                //Upvotes the selected post
+                posts.upvote(forum_thread, post);
+            };
+            $scope.incrementDownvotes = function (forum_thread, post) {
+                //Downvotes the selected post
+                posts.downvote(forum_thread, post);
+            };
+            $scope.isLoggedIn = function () {
+                //Checks if there is a user logged in
+                return Auth.isAuthenticated();
+            };
+            $scope.pagesArray = function (thread) {
+                //Return the array for page navigation
+                var pages = [];
+                var pageCount = Math.floor((thread.posts.length - 1) / 10) + 1;
+                if (pageCount > 6 && $scope.current_page < 3) {
+                    pages = [1, 2, 3, "...", pageCount];
+                } else if (pageCount > 6 && $scope.current_page == 3) {
+                    pages = [1, 2, 3, 4, "...", pageCount];
+                } else if (pageCount > 6 && $scope.current_page >= 4 && $scope.current_page < pageCount - 2) {
+                    pages = [1, "...", $scope.current_page - 1, $scope.current_page, parseInt($scope.current_page) + 1, "...", pageCount];
+                } else if (pageCount > 6 && $scope.current_page == pageCount - 2) {
+                    pages = [1, "...", $scope.current_page - 1, pageCount - 2, pageCount - 1, pageCount];
+                } else if (pageCount > 6 && $scope.current_page > pageCount - 2) {
+                    pages = [1, "...", pageCount - 2, pageCount - 1, pageCount];
+                } else {
+                    for (var i = 1; i <= pageCount; i++) {
+                        pages.push(i);
+                    }
+                }
+                return pages;
+            };
+            $scope.showPosts = function (container) {
+                //Helper function for showing hidden posts
+                $scope.showAllPosts = true;
+            };
+        }
+    ]);
--- app/assets/javascripts/forum_threads/forum_threads.js
+++ app/assets/javascripts/forum_threads/forum_threads.js
@@ -1,25 +1,32 @@
 angular.module('stoutForum')
-.factory('forum_threads', [
-  '$http',
-  function($http){
-  	var o = { forum_threads: [] };
-  	o.getAll = function(sub_forum, page) {
-      return $http.get('/sub_forums/' + sub_forum + '/forum_threads/index/' + page + '.json').success(function(data){
-        angular.copy(data, o.forum_threads)
-      })
-    };
-    o.get = function(id, sub_forum) {
-  	  return $http.get('/sub_forums/' + sub_forum + '/forum_threads/' + id + '.json').then(function(res){
-  	  	return res.data;
-  	  })
-  	};
-    o.create = function(thread, sub_forum) {
-      return $http.post('/sub_forums/' + sub_forum._id + '/forum_threads.json', thread).success(function(data){
-         console.log(data);
-         var newURL = '#/sub_forums/' + sub_forum._id + '/1'
-         window.location.href = newURL;
-         window.location.reload(newURL);
-      });
-    };
-  	return o;
-  }]);
+    .factory('forum_threads', [
+        '$http',
+        function ($http) {
+            //Initialize an array to hold the paginated ForumThreads
+            var o = {forum_threads: []};
+            o.getAll = function (sub_forum, page) {
+                //Call the API to retrieve all the forum threads for a SubForum
+                return $http.get('/sub_forums/' + sub_forum + '/forum_threads/index/' + page + '.json').success(function (data) {
+                    //Add the results to the threads array
+                    angular.copy(data, o.forum_threads);
+                });
+            };
+            o.get = function (id, sub_forum) {
+                //Call the API to retrieve a single forum thread by id
+                return $http.get('/sub_forums/' + sub_forum + '/forum_threads/' + id + '.json').then(function (res) {
+                    //Return the thread
+                    return res.data;
+                });
+            };
+            o.create = function (thread, sub_forum) {
+                //Call the API to retrieve a single forum thread by id
+                return $http.post('/sub_forums/' + sub_forum._id + '/forum_threads.json', thread).success(function (data) {
+                    //Navigate to the page where the new thread is located
+                    var newURL = '#/sub_forums/' + sub_forum._id + '/1';
+                    window.location.href = newURL;
+                    window.location.reload(newURL);
+                });
+            };
+            return o;
+        }
+    ]);
